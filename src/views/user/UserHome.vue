f2b49646825a37715b711287ce7b3562c08630e7
JimChen <tc21122402@tzuchi.com.tw>
2024-03-18 09:46:44 +0800

    (dollob) 新增 header & footer

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -1,8 +1,31 @@
 <template>
-    <h1>首頁導覽列</h1>
+    <!-- <h1>首頁導覽列</h1>
         <RouterLink to="/">首頁</RouterLink> |
         <RouterLink to="/products">產品列表</RouterLink> |
         <RouterLink to="/cart">購物車列表</RouterLink> |
-        <RouterLink to="/admin">登入後台</RouterLink>
+        <RouterLink to="/admin">登入後台</RouterLink> -->
+    <header>
+        <user-header></user-header>
+    </header>
     <RouterView />
+    <footer>
+        <user-footer></user-footer>
+    </footer>
 </template>
+
+<script>
+import UserHeader from '@/components/UserHeader.vue'
+import UserFooter from '@/components/UserFooter.vue'
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    UserHeader,
+    UserFooter
+  }
+}
+</script>
