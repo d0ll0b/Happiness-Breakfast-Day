8aabdeb20ed312ba13fabe4e718221ed8b8a2684
dollob <s1052782@gm.pu.edu.tw>
2024-05-20 23:47:32 +0800

    (dollob) 貼文後台設置

--- src/views/admin/AdminPosts.vue
+++ src/views/admin/AdminPosts.vue
@@ -1,8 +1,18 @@
 <template>
     <h1 class="mt-3">貼文頁面</h1>
-    <VueLoading :active="isLoading" />
+    <VueLoading :active="isLoading">
+      <fairy-loading></fairy-loading>
+    </VueLoading>
     <div class="container">
         <div class="mt-4">
+            <!-- 刪除Modal -->
+            <del-modal ref="DelModal" :temp="product" @delproduct="Delete_product"></del-modal>
+            <!-- 刪除Modal -->
+
+            <!-- 訊息 -->
+            <alert-messages ref="AlertMessages"></alert-messages>
+            <!-- 訊息 -->
+
             <div class="text-end">
                 <button class="btn btn-primary" type="button" @click="delete_cart()">新增貼文</button>
             </div>
@@ -34,11 +44,49 @@
                 </tbody>
             </table>
         </div>
+
+        <!-- Pagination -->
+        <pagination-btn :pagination="pagination" @change_page="getData"></pagination-btn>
+        <!-- Pagination -->
     </div>
 </template>
 
 <script>
-export default {
+import DelModal from '@/components/DelModal.vue'
+import AlertMessages from '@/components/AlertMessages.vue'
+import PaginationBtn from '@/components/PaginationBtn.vue'
+import FairyLoading from '@/components/FairyLoading.vue'
+const { VITE_APP_API_URL: apiUrl, VITE_APP_API_NAME: apiPath } = import.meta.env
 
+export default {
+  data () {
+    return {
+      articles: [],
+      pagination: {},
+      isNew: false,
+      isLoading: false
+    }
+  },
+  components: {
+    DelModal,
+    AlertMessages,
+    PaginationBtn,
+    FairyLoading
+  },
+  methods: {
+    getArticles (page = 1) {
+      this.isLoading = true
+      const api = `${apiUrl}/api/${apiPath}/admin/articles?page=${page}`
+      this.axios.get(api).then((res) => {
+        const { articles, pagination } = res.data
+        this.articles = articles
+        this.pagination = pagination
+      }).catch((err) => {
+        this.$refs.AlertMessages.show_alert(err?.response.data.message, 1300, 'error')
+      }).finally(() => {
+        this.isLoading = false
+      })
+    }
+  }
 }
 </script>
