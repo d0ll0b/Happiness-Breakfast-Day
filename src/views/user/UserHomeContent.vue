ccb68242a2252864c839ce68c0f6eb354916e16e
dollob <s1052782@gm.pu.edu.tw>
2024-06-01 00:03:22 +0800

    (dollob) 首頁新增scrollTopBtn

--- src/views/user/UserHomeContent.vue
+++ src/views/user/UserHomeContent.vue
@@ -147,6 +147,14 @@
     <!-- location -->
   </div>
 
+  <div>
+    <button class="btn btn-sm scrollTopBtn text-light">
+      <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-caret-up-square-fill" viewBox="0 0 16 16">
+        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4 9h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5A.5.5 0 0 0 4 11"/>
+      </svg>
+    </button>
+  </div>
+
 </template>
 
 <script>
